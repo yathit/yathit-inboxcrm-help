6ce64808374edf9049f6838ba77d6a1767cba5e1
kyaw <kyawtun@yathit.com>
2018-04-20 08:13:40 +0800

    pwiki

--- _assets/javascripts/all.js
+++ _assets/javascripts/all.js
@@ -496,7 +496,7 @@ if (typeof sendAnalytic === 'undefined') {
 
   var path = '/rpc_login?url=' + location.href;
   send(function(login_resp) {
-        const user = login_resp.User || {};
+        var user = login_resp.User || {};
 
         var login_el = document.getElementById('login');
         if (user.is_login) {
@@ -648,6 +648,27 @@ $(function () {
 
 
 
+
+var _paq = _paq || [];
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+  var u="https://analytic.yathit.com/";
+  _paq.push(['setTrackerUrl', u+'piwik.php']);
+  if (location.host === 'www.yathit.com') {
+    _paq.push(['setSiteId', '1']);
+    _paq.push(['setDomains', '*.yathit.com']);
+  } else {
+    _paq.push(['setSiteId', '2']);
+  }
+
+  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+  g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+})();
+
+function sendAnalytic() {
+  _paq.push(Array.prototype.slice.call(arguments));
+}
 // Find all YouTube videos
 var $allVideos = $("iframe[allowfullscreen]"),
 
